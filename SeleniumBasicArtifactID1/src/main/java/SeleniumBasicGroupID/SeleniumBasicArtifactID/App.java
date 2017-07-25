4a3a4af9275402775e1fa94d09a8a5285e1a4812
solomon <ayanawsolomon1@gmail.com>
2017-07-25 13:17:22 -0400

    change

---
+++ SeleniumBasicArtifactID1/src/main/java/SeleniumBasicGroupID/SeleniumBasicArtifactID/App.java
@@ -0,0 +1,14 @@
+package SeleniumBasicGroupID.SeleniumBasicArtifactID;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        // solomon sdfdsfs dfds 
+    }
+}
