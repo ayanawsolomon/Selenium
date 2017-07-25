8327c6005f7c29b1f411699ac9ec7f46c23166e6
solomon <ayanawsolomon1@gmail.com>
2017-07-25 12:34:42 -0400

    adding comment

---
+++ BDDArtifactID/src/test/java/BDDGroupIID/BDDArtifactID/Drivers.java
@@ -0,0 +1,12 @@
+package BDDGroupIID.BDDArtifactID;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.Before;
+
+public class Drivers {
+	
+}
